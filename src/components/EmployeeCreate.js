6988193f4aaf10ec962152b3b9523195bdd1ed40
guruganesh <guru96299@gmail.com>
2017-03-29 20:56:45 +0530

    new employee added and routed

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+	
+	onButtonPress() {
+		const { name, phone, shift } = this.props;
+
+		this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+	}
+
+
 	render() {
 		return (
 			<Card>
@@ -19,13 +27,14 @@ class EmployeeCreate extends Component {
 
 				<CardSection>
 					<Input
-					label="phone"
+					label="Phone"
 					placeholder="96299 87793"
 					value={this.props.phone}
 					onChangeText= {value => this.props.employeeUpdate({prop:'phone', value })}
 					/>
 				</CardSection>
-				<CardSection>
+				<CardSection style={{flexDirection:'row',height:60, alignItems:'center'}}>
+				<Text style={styles.pickerTextStyle}> Shift </Text>
 					<Picker
 					 	style={{flex:1}}
 						selectedValue ={this.props.shift}
@@ -42,12 +51,24 @@ class EmployeeCreate extends Component {
 					</Picker>
 				</CardSection>
 				<CardSection>
-					<Button>
+					<Button
+						onPress={this.onButtonPress.bind(this)}
+					>
 						Create
 					</Button>
 				</CardSection>
 			</Card>
 		);
+	}
+}
+
+const styles = {
+	pickerTextStyle: {
+	fontSize: 18,
+	paddingLeft: 15,
+	flex:1,
+	height:60,
+	alignItems:'center'
 	}
 }
 
@@ -58,4 +79,4 @@ const mapStateToProps = (state) => {
 
 };
 
-export default connect (mapStateToProps, {employeeUpdate})(EmployeeCreate);
+export default connect (mapStateToProps, {employeeUpdate, employeeCreate})(EmployeeCreate);
